1c5cacf1835608a6e1913833e4bb4a328f24e6e2
Victor Hugo Belinello <victorbelinello17@gmail.com>
2020-08-14 11:02:29 -0300

    Small header refactor.

--- App01/Python/multicast_peer.py
+++ App01/Python/multicast_peer.py
@@ -1,62 +1,75 @@
 import socket
 import struct
-from uuid import uuid4
 from threading import Thread
+from uuid import uuid4
 
-import crypt
-
-
-multicast_addr = '224.0.0.0'
-bind_addr = '0.0.0.0' # Bind to all interfaces in the system
-port = 3000
-
-
+import crypto
+from setup import *
 
 class MulticastPeer(Thread):
-    
+
     def __init__(self):
         Thread.__init__(self)
 
-        #### Network 
+        # Network
         # Socket para IPv4, UDP(Datagramas)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Configuração para socket multicast (não tem uma classe especifica como em Java)
-        membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
-        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+        membership = socket.inet_aton(
+            MULTICAST_ADDR) + socket.inet_aton(BIND_ADDR)
+        self.sock.setsockopt(
+            socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        self.sock.bind((bind_addr, port))
+        self.sock.bind((BIND_ADDR, PORT))
 
-        #### Thread
+        # Thread
         # Inicia thread de leitura de mensagens
         self.start()
 
-        #### Peer object
+        # Peer object
         self.pubKeys = {}
         # Obtem um uuid aleatorio
         self.UUID = uuid4()
-        # Obtem par de chaves 
-        self.keyPair = crypt.getKeys()
+        # Obtem par de chaves
+        self.keyPair = crypto.getKeys()
+        pubKey = self.keyPair.publickey()
+
         # Adiciona chave publica
-        self.pubKeys[str(self.UUID)] = self.keyPair.publickey()
-        
+        self.pubKeys[str(self.UUID)] = pubKey
+
+        # Envia chave publica para grupo Multicast
+        self.sendMulticast("Teste", "0")
+        #self.sendMulticast(str(pubKey), "0")
+        exit(0)
 
     def getInput(self):
         while True:
             msg = input("Digite a mensagem: ")
             self.sendMulticast(msg)
 
-    # Envia msg para grupo multicast
-    def sendMulticast(self, msg):
-        # Obtem assinatura da msg
-        sig = crypt.sign(msg.encode("UTF-8"), self.keyPair)
+    def addHeader(self, msg, msgType):
+        # Obtem comprimento da mensagem e converte para string
+        msgSize = str(len(msg))
+        # zfill preenche com zeros a esquerda para que a string tenha tamanho final MAX_NUMBER_DIGITS_MSG
+        msgSize = msgSize.zfill(MAX_NUMBER_DIGITS_MSG)
         # Adiciona header na msg
-        msg = str(self.UUID) + msg 
+        msg = str(self.UUID) + msgType + msgSize + msg
+        print(f"UUID:{msg[UUID_RANGE]}\nType:{msg[MSG_TYPE_RANGE]}\nLenght:{msg[MSG_LENGHT_RANGE]}\nMessage:{msg[MSG_START_BYTE:]}")
+        return msg
+
+    # Envia msg para grupo multicast
+    def sendMulticast(self, msg, msgType):
+        # Obtem assinatura da msg
+        sig = crypto.sign(msg.encode("UTF-8"), self.keyPair)
+
+        msg = self.addHeader(msg, msgType)
+        exit(0)
         # Combina mensagem e assinatura em bytes
         data = msg.encode("UTF-8") + sig
         # Envia para grupo
-        self.sock.sendto(data, (multicast_addr, port))        
+        self.sock.sendto(data, (multicast_addr, port))
 
     def run(self):
         while True:
@@ -66,13 +79,14 @@ class MulticastPeer(Thread):
             senderID = data[:36].decode('UTF-8')
             # Obtem chave publica
            # pubKey = pubKeys[senderID]
-            # Extrai mensagem 
-            msg = data[36:36+5] # TODO: deixar generico, so funciona com msg = Teste
+            # Extrai mensagem
+            # TODO: deixar generico, so funciona com msg = Teste
+            msg = data[36:]
             # Extra assinatura da mensagem
             sig = bytes(data[36+5:])
 
             # Verifica assinatura
-            #if(crypt.verify(pubKey, msg, sig)):
+            # if(crypt.verify(pubKey, msg, sig)):
             #    print("Deu boa")
             print(f"\n[{senderID}]: {msg.decode()}\n")
            # else:
